c75e24f10b3d80b343fdae9dda28b18f0e6e6ef9
ZejunZhou <zzhou443>
2023-08-03 15:56:55 +0800

    updating requirement documentation

--- react-app/src/configData.js
+++ react-app/src/configData.js
@@ -1,4 +1,4 @@
 export default {
-  CLIENT_ID: '',
-  GPT_API_KEY: "",
+  CLIENT_ID: '322785260688-dqs9rla37jqgljqq683mqqpif4v25ke0.apps.googleusercontent.com',
+  GPT_API_KEY: "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",
 };
